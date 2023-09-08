bee1eb6b5521f0648530963d6232763aab4c2411
vitordbo <vitorduarte177@gmail.com>
2023-09-08 09:17:04 -0300

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:tell_craft/features/login/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:tell_craft/firebase/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
