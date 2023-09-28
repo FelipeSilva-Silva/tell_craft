56f32af39e70a44d4c575466802fc9c04f0a74e5
JFelipeBS <f.filipisilva13@hotmail.com>
2023-09-28 10:52:08 -0300

    setting and generator

--- lib/features/generator_new_history.dart
+++ lib/features/generator_new_history.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
 
 class GeneratorHistoryPage extends StatefulWidget {
   const GeneratorHistoryPage({super.key});
@@ -8,18 +9,106 @@ class GeneratorHistoryPage extends StatefulWidget {
 }
 
 class _GeneratorHistoryPageState extends State<GeneratorHistoryPage> {
+  final _quantityCharacters = TextEditingController();
+  final _namescharacters = TextEditingController();
+  final _theme = TextEditingController();
+  final _local = TextEditingController();
+
+  @override
+  void dispose() {
+    _quantityCharacters.dispose();
+    _namescharacters.dispose();
+    _theme.dispose();
+    _local.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
         title: const Text('Crie Sua História'),
         centerTitle: true,
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(8),
-        child: Form(
-          child: SingleChildScrollView(
-            child: Column(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Form(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //quantidade de personagens
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: TextFormField(
+                      validator: Validatorless.required('Campo obrigatório'),
+                      controller: _quantityCharacters,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                          label: Text('Quantos personagens tem sua história?')),
+                    ),
+                  ),
+                  //nomes dos personagens
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: TextFormField(
+                      validator: Validatorless.required('Campo obrigatório'),
+                      controller: _namescharacters,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                          label: Text(
+                              'Nomes dos personagens separados por virgula')),
+                    ),
+                  ),
+                  //tema
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: TextFormField(
+                      validator: Validatorless.required('Campo obrigatório'),
+                      controller: _theme,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                          label: Text('Qual o tema? ex: Fantasia, distopia')),
+                    ),
+                  ),
+                  //local
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: TextFormField(
+                      validator: Validatorless.required('Campo obrigatório'),
+                      controller: _local,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                          label: Text('Nome do local? ex: Reino do repolho')),
+                    ),
+                  ),
+                  //botao
+                  Padding(
+                    padding: const EdgeInsets.only(top: 100),
+                    child: SizedBox(
+                      height: 50,
+                      width: 200,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStatePropertyAll(Colors.amber[600])),
+                        child: const Text(
+                          'Criar História',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- lib/features/setting/setting_page.dart
+++ lib/features/setting/setting_page.dart
@@ -1,16 +1,144 @@
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final name = TextEditingController();
+    final email = TextEditingController();
+    final senha = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: const Text('Configurações'),
         centerTitle: true,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: name,
+              enabled: false,
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  label: Text('name'),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ))),
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              controller: email,
+              enabled: false,
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  label: Text('Email'),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ))),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Colors.amber[600])),
+                  onPressed: () => _showMyDialog(context),
+                  child: const Text('Alterar senha'),
+                ),
+                const SizedBox(width: 25),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Colors.red[700])),
+                  onPressed: () {},
+                  child: const Text('Sair'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
+}
+
+Future<void> _showMyDialog(BuildContext context) async {
+  return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Aterar Senha'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                TextFormField(
+                  validator: Validatorless.multiple([
+                    Validatorless.required('Senha é obrigatório'),
+                  ]),
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    border: UnderlineInputBorder(),
+                    labelText: 'Nova Senha',
+                  ),
+                ),
+                const SizedBox(height: 15),
+                TextFormField(
+                  validator: Validatorless.multiple([
+                    Validatorless.required('Senha é obrigatório'),
+                  ]),
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    border: UnderlineInputBorder(),
+                    labelText: 'Confirma Nova Senha',
+                  ),
+                ),
+                const SizedBox(height: 15),
+                const Text('Confirme com sua senha atual'),
+                const SizedBox(height: 15),
+                TextFormField(
+                  validator: Validatorless.multiple([
+                    Validatorless.required('Senha é obrigatório'),
+                  ]),
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    border: UnderlineInputBorder(),
+                    labelText: 'Senha',
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: const Text('Alterar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      });
 }
